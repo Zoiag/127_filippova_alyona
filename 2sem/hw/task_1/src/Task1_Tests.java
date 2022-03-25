3281a54cc1706d37269b250861b97973ae88659e
Zoiag <57074412+Zoiag@users.noreply.github.com>
2022-03-25 15:06:44 +0300

    Создала новый тест Check_DoubleLinkedList_pushBack

--- 2sem/hw/task_1/src/Task1_Tests.java
+++ 2sem/hw/task_1/src/Task1_Tests.java
@@ -5,4 +5,10 @@ public class Task1_Tests extends Assert{
         DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
         assertTrue(list.isEmpty());
     }
+    @Test
+    public void Check_DoubleLinkedList_pushBack(){
+        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
+        list.pushBack(1);
+        assertFalse(list.isEmpty());
+    }
 }
